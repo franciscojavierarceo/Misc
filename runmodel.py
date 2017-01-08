542da6a803cb59e674e04572a7d8a1e447c3c07e
Francisco Javier Arceo <franciscojavierarceo@Franciscos-MacBook-Pro.local>
2017-01-07 22:16:42 -0500

    Updated model and code

---
+++ runmodel.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import os
+import sys
+import train
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.externals import joblib
+
+def main(traindatapth, testdatapath, inputfile):
+	if 'ad_classifier.pkl' not in os.listdir(testdatapath):
+		print("Training model")
+		train.main(traindatapth, meanvaldf=None)
+	else:
+		print("Loading pretrained model located %s" % os.path.join(testdatapath, 'ad_classifier.pkl') )
+
+	meanvaldf = pd.read_csv(os.path.join(testdatapath, 'inputmeanvalues.csv'), index_col=0)
+	colfile = 'column.names.txt'
+
+	# Reading in dataset
+	dfx = pd.read_csv(os.path.join(traindatapth, inputfile), header=None)
+	df2 = pd.read_csv(os.path.join(traindatapth, colfile), sep=":", skiprows=0)
+	df2 = df2.reset_index()
+	df2.columns = ['variable', 'type']
+
+	if dfx.shape[1] == (df2.shape[0] + 1):
+		dfx.columns = df2['variable'].tolist() + ['ad']
+
+	df1 = train.cleandata(dfx, None, meanvaldf)
+
+	nbmodel = joblib.load('ad_classifier.pkl')
+
+	xcols = [col for col in df1.columns if col !='ad']
+	X_train= df1[xcols].values
+
+	# Exporting predctions
+	preds = pd.DataFrame(nbmodel.predict(X_train)  , columns=['preds'])
+	preds.to_csv("./preds.csv")
+	print("Predictions exported to ./preds.csv")
+
+if __name__ == "__main__":
+	main(sys.argv[1], sys.argv[2], sys.argv[3])
--- train.py
+++ train.py
@@ -1,49 +1,86 @@
+#!/usr/bin/python
 import os
+import sys
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 import sklearn
 from sklearn.externals import joblib
 from sklearn.linear_model import LassoCV
-from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import roc_auc_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 
-pth = './datafiles/'
-inputfile = 'data'
-colfile = 'column.names.txt'
+# Need to run this using:
+#  python train.py ./datafiles/
 
-mymodel = joblib.load('ad_classifier.pkl')
+def cleandata(inputdf, trainfilt, meanvaldf):
+	xdf = inputdf
+	# Converting the columns that were read as character to numeric columns
+	for col in xdf.columns:
+		if xdf[col].dtype not in (float, int) and col!='ad':
+			xdf.ix[xdf[col].str.strip()=='?', col] = np.nan
+			xdf[col] = xdf[col].astype(float)
 
-df1 = pd.read_csv(os.path.join(pth, inputfile), header=None)
-df2 = pd.read_csv(os.path.join(pth, colfile), sep=":", skiprows=0)
-df2 = df2.reset_index()
-df2.columns = ['variable', 'type']
+	# Replacing the unknown values with missing binary/indicator variables
+	for col in xdf.columns:
+		if xdf[col].dtype in (float, int) and col!='ad':
+			xdf[col+'missing'] = xdf[col].isnull().astype(int)
+			if xdf[col].nunique() ==2:
+				xdf[col] = xdf[col].fillna(0)
+			else:
+				if meanvaldf is None:
+					meanval = xdf.ix[xdf[col].notnull() & trainfilt, col].mean()
+				else:
+					meanval = meanvaldf[col]['mean']
 
-df1.columns = df2['variable'].tolist() + ['ad']
+				xdf[col] = xdf[col].fillna(meanval)
+	return xdf
 
-for col in df1.columns:
-    if df1[col].dtype not in (float, int) and col!='ad':
-        df1.ix[df1[col].str.strip()=='?', col] = np.nan
-        df1[col] = df1[col].astype(float)
+def main(pth, meanvaldf):
+	np.random.seed(seed=100)
+	inputfile = 'data'
+	colfile = 'column.names.txt'
 
-rans = np.random.rand(df1.shape[0]) 
-trainfilt = rans <= (1/3.)
-validfilt = (rans > (1/3.) ) & (rans <= (2/3.))
-testfilt = rans > (2/3.)
+	# Reading in dataset
+	dfx = pd.read_csv(os.path.join(pth, inputfile), header=None)
+	df2 = pd.read_csv(os.path.join(pth, colfile), sep=":", skiprows=0)
+	df2 = df2.reset_index()
+	df2.columns = ['variable', 'type']
+	dfx.columns = df2['variable'].tolist() + ['ad']
+	trainfilt = np.ones(dfx.shape[0]) == 1
 
+	df1 = cleandata(dfx, trainfilt, meanvaldf)
 
-for col in df1.columns:
-    if df1[col].dtype in (float, int) and col!='ad':
-        df1[col+'missing'] = df1[col].isnull().astype(int)
-        if df1[col].nunique() ==2:
-            df1[col] = df1[col].fillna(0)
-        else:
-            meanval = df1.ix[df1[col].notnull() & trainfilt, col].mean()
-            df1[col] = df1[col].fillna(meanval)
+	if meanvaldf == None:
+		meanvals = df1.describe()
+		meanvals.to_csv('./inputmeanvalues.csv')
 
+	print("Writing summary statistics to ./inputmeanvalues.csv")
 
-ys = (df1['ad'].str.strip() == 'ad.' ).astype(int).values
-xcols = [col for col in df1.columns if col !='ad']
-X_train, X_valid, X_test = df1.ix[trainfilt, xcols].values, df1.ix[validfilt, xcols].values, df1.ix[testfilt, xcols].values
-y_train, y_valid, y_test = ys[trainfilt], ys[validfilt], ys[testfilt]
+	ys = (df1['ad'].str.strip() == 'ad.' ).astype(int).values
+	xcols = [col for col in df1.columns if col !='ad']
+	X_train, y_train= df1.ix[trainfilt, xcols].values, ys[trainfilt]
+
+	nbmodel = BernoulliNB() 
+	nbmodel.fit(X_train, y_train)
+
+	nbclass = nbmodel.predict(X_train)  
+	nbproba = nbmodel.predict_proba(X_train)[:, 1]
+
+	# Choosing the model with the best performance on the validation data, which happens to be the Naive Bayes
+	print("Train Naive Bayes AUC = %.6f" % roc_auc_score(y_train, nbproba))
+	print("Train Naive Bayes Accuracy = %.6f" %  np.mean(y_train==nbclass) )
+
+	# Confusion Matrix
+	print("Training Confusion Matrix")
+	print(confusion_matrix(y_train, nbclass))
+
+	print("Writing model to ./ad_classifier.pkl" )
+	joblib.dump(nbmodel, 'ad_classifier.pkl') 
+
+if __name__ == "__main__":
+	path = sys.argv[1]
+	main(pth=path, meanvaldf=None)
